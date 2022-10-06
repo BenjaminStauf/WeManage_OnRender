7e9e2ed2292c2c6447a079f121fa8d2038f22846
LukasSemler <semler.l04@htlwienwest.at>
2022-10-06 08:25:18 +0200

    vlt jetzt

--- app.js
+++ app.js
@@ -31,4 +31,4 @@ app.use(notFoundHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`server running on port ${PORT}`.bgMagenta));
